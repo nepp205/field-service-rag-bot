f135ec3103cf9a74748f42de9bd6224d434764be
Marvxn23 <marvin.palsbroeker@gmail.com>
2026-04-07 13:09:18 +0200

    added test file for request

---
+++ Context_Handler/test_request.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_TOKEN = os.getenv("WEBSERVER_TOKEN")
+
+url = "http://localhost:5000/context"
+token = SECRET_TOKEN
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
+data = {
+    "query": "Mein Geschirrspüler zeigt den Fehler F-404 an was soll ich tun?",
+    "model": "pdr 508"  # Optional: Filtern nach Modellname
+}
+response = requests.post(url, headers=headers, json=data)
+print(json.dumps(response.json(), indent=2))
