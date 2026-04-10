286d5fd102787cbf0f55daa6d675304fe203d750
Marvxn23 <marvin.palsbroeker@gmail.com>
2026-04-10 17:22:02 +0200

    fix: update Dockerfile comments for clarity and adjust test_request.py query for debugging

--- Context_Handler/test_request.py
+++ Context_Handler/test_request.py
@@ -1,3 +1,5 @@
+# für diesen test muss der Docker container des context handlers laufen
+# test für modulares debugging und testen
 import requests
 import json
 from dotenv import load_dotenv
@@ -14,8 +16,8 @@ headers = {
     "Content-Type": "application/json"
 }
 data = {
-    # "query": "Mein Geschirrspüler zeigt den Fehler F-404 an was soll ich tun?",
-    "query": "Hab Fehler bei mein Produkt brauch Hilfe?",
+    "query": "Mein Geschirrspüler zeigt den Fehler F-404 an was soll ich tun?",
+    # "query": "Hab Fehler bei mein Produkt brauch Hilfe?",
     "model": "pdr 508"  # Optional: Filtern nach Modellname
 }
 response = requests.post(url, headers=headers, json=data)
